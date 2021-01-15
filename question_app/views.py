7d15b706bf19e8c9bb21725bbdf54e9ed551b256
Shubhansh <shubhansh7777@gmail.com>
2021-01-15 16:38:53 +0530

    User can't play the game twice once he/she completed.
    Road to modify the chart js and link the result page with the analysis page.

--- question_app/views.py
+++ question_app/views.py
@@ -1,8 +1,6 @@
 from django.views.generic           import TemplateView 
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth            import get_user_model
 from django.contrib.auth.mixins     import LoginRequiredMixin
-
 from django.contrib                 import messages
 from django.http                    import Http404
 from django.shortcuts               import get_object_or_404,render,redirect
@@ -25,25 +23,30 @@ def QuestionPage( request, question_id=None ):
     '''Show Detail Page with messages.'''
 
     if type(question_id):
-        try:
-            obj_numbering = Numbering.objects.get( question_number=question_id )
+        if not request.user.is_complete_quiz:
+            try:
+                obj_numbering = Numbering.objects.get( question_number=question_id )
 
-        except Numbering.DoesNotExist:
-            '''Extra work for result page.'''
+            except Numbering.DoesNotExist:
+                '''Extra work for result page.'''
 
-            if question_id == 0:
+                if question_id == 0:
 
-                '''is_complete_quiz have to true'''
-                #request.user.is_complete_quiz = True 
-                return redirect( 'result' ) 
+                    '''is_complete_quiz have to true'''
+                    request.user.is_complete_quiz = True 
+                    request.user.save()
+                    return redirect( 'result' ) 
 
-            raise Http404('Question Not Found')
+                raise Http404('Question Not Found')
 
-        obj           = get_object_or_404( Question,  question_number=obj_numbering )
-        context       = { 'question' : obj }
-        return render( request , 'question.html' ,context=context)
-    else:
-        raise Http404('Question Not Found')
+            question_obj  = get_object_or_404( Question,  question_number=obj_numbering )
+            context       = { 'question' : question_obj,
+                              'passing_pushes' : Question.objects.passing_pushes 
+                            }
+
+            return render( request , 'question.html' ,context=context)
+        raise Http404('You can\'t play the game twice.')
+    raise Http404('Question Not Found')
 
 
 @login_required()
@@ -54,39 +57,38 @@ def FormProcessing( request, question_id=None ):
         answer     = request.POST.get('image_selected') 
 
         obj_numbering = get_object_or_404( Numbering, question_number=question_id )
-        obj           = get_object_or_404( Question, question_number=obj_numbering )
-        user_login    = get_object_or_404( get_user_model(), pk=request.user.id  )
+        question_obj  = get_object_or_404( Question, question_number=obj_numbering )
 
         '''Comparision with answer'''
         if answer in ['A','B','C','D']:
 
             #When the answer is wrong
-            if not obj.answer == answer:
+            if not question_obj.answer == answer:
                 messages.error( request,
-                                'Gand Maraliye, Ans was %s' %(obj.answer) 
+                                'Gand Maraliye, Ans was %s' %(question_obj.answer) 
                               )
             
             #When the answer is correct
             else:
-                user_login.increase_winning_prize
-                user_login.increase_correct_answers
+                request.user.increase_winning_prize
+                request.user.increase_correct_answers
                 messages.success( request,
                                   'Sahi Jawab!'
                                 )
 
         #When the question is skipped
         else:
-            messages.info( request,'Aapki phat k chaar!')
+            messages.warning( request,'Aapki gand phat gyi lgta')
             
             # skip question many2many
-            user_login.skip_question.add( obj ) 
+            request.user.skip_question.add( question_obj ) 
 
-        user_login.save()
+        request.user.save()
         
+        # Next question
         return redirect( 'question', question_id=question_id-1 )
 
-    else:
-        raise Http404('Invalid Data.')
+    raise Http404('Invalid Data.')
 
 
 class ResultView( LoginRequiredMixin,TemplateView ):
@@ -96,6 +98,6 @@ class ResultView( LoginRequiredMixin,TemplateView ):
 
     def get_context_data( self,**kwargs ):
         context                     = super().get_context_data( **kwargs )
-        context['total_questions']   = Question.objects.all().count()
+        context['total_questions']  = Question.objects.total_questions
         return context
  
