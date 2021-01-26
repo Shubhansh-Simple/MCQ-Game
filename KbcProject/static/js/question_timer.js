c76d0d992e4ab1d7c1e2e1b5020dec4a52b16763
Shubhansh <shubhansh7777@gmail.com>
2021-01-26 13:12:38 +0530

    Adding a timer to Question Template
    Excellent way to add "js" code in our template.
    .) Only question template have this js timer.
    .) Only non-attempt question have this js timer.
    .) Timer not starts from initial state on refresh.
    .) Timer only reset if the user goes to next page.
    .) If the timer ends question automatically skipped.

--- KbcProject/static/js/question_timer.js
+++ KbcProject/static/js/question_timer.js
@@ -1,22 +1,64 @@
+/*
+  * Js file specially for question template
+  * checks whether the user refresh / go to next question
+  * refresh       - don't change the timer
+  * next question - timer starts with initial state
+  *
+  *
+*/
+function removeKey(){
+  // reset the localStorage's value
+  //
+  try{
+    localStorage.setItem( 'question_id_key', current_question_id )
+    localStorage.setItem( 'timer_value_key', 30 )
+  } catch ( e ) {
+    // do sometihng here if it's fail
+    // redirect to the error page.
+  }
+}
+
+
 document.addEventListener('DOMContentLoaded', function(){
+  
+  current_question_id = document.getElementById('question_number_id_html').innerText 
+  
+  if ( localStorage.getItem('question_id_key') 
+       && 
+       localStorage.getItem('timer_value_key')  ){
+     // Storage exist then,
 
-    // we wait for this seconds to skip the question
-    var time=30;
-    var timer_variable = setInterval( function(){
+     if ( localStorage.getItem('question_id_key') !==  current_question_id ){
+       // user redirect to another question
+       // we are updating the localStorage value
+       removeKey();
+     }
+  }
+  else{
+    // creating localStorage
+    removeKey();
+  }
 
-       if (time <= 0){
-         clearInterval( timer_variable );
-         document.getElementById('skip_button_id').click() 
-       }
+  // we wait for this seconds to skip the question
+  var time = localStorage.getItem('timer_value_key');
 
-       document.getElementById('timer_count').innerHTML=time
-       time -= 1
+  var timer_variable = setInterval( function(){
+     // in every 1000 milliseconds this section executes
+     //
+     if (time <= 0){
+       clearInterval( timer_variable );
+       removeKey();
+       document.getElementById('skip_button_id').click();
+     }
 
-    },1000 );
-  },false
-)
+     document.getElementById('timer_count').innerHTML=time
+     time -= 1
+     localStorage.setItem( 'timer_value_key',time );
 
+  },1000 );
 
+ },false // function ends
+)  // DOMContentLoaded ended
 
 
 
